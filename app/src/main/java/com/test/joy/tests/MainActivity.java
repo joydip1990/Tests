724590d1da3d6e2ccda07d5dd03192c0ba93e9a7
Joydip Pal <pal.joydip@gmail.com>
2015-04-29 13:03:26 +0530

    test

--- app/src/main/java/com/test/joy/tests/MainActivity.java
+++ app/src/main/java/com/test/joy/tests/MainActivity.java
@@ -7,11 +7,12 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-int i=0;
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // hello java.
     }
 
 }
